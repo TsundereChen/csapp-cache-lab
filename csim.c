aeed6118c0878f030e9fcb9d2ac0bc3a35dbc4a9
Tsundere Chen <me@tsunderechen.io>
2021-01-12 00:54:07 +0800

    fix(csim.c): Rewrite simulator according to previous work
    Rewrite the simulator and cacheCheck function according to the work written in https://hackmd.io/@pSnFKx_GTlmTWXn4A8lpKw/SkeA19jWN.

--- csim.c
+++ csim.c
@@ -13,7 +13,9 @@ typedef struct cacheData {
   uint64_t tag;
 } cacheData;
 
-cacheData **cacheTable;
+typedef cacheData* cacheSet;
+typedef cacheSet* cacheTable;
+cacheTable cache;
 
 bool verbose = false;
 int setIndexBit;
@@ -27,7 +29,10 @@ int evictions = 0;
 
 void parseArg(int argc, char *argv[]);
 int simulator(void);
-int checkCache(uint64_t address);
+void checkCache(int tag, int cacheSet, int offset, int counter);
+unsigned int parseTag(int setIndexBit, int blockOffsetBit, int address);
+unsigned int parseCacheSet(int setIndexBit, int blockOffsetBit, int address);
+unsigned int parseOffset(int setIndexBit, int blockOffsetBit, int address);
 
 int main(int argc, char *argv[]) {
   parseArg(argc, argv);
@@ -37,11 +42,15 @@ int main(int argc, char *argv[]) {
 }
 
 void parseArg(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    exit(1);
+  };
   int opt;
   while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
     switch (opt) {
     case 'h':
-      printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+      printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
       printf("Options:\n");
       printf("  -h         Print this help message.\n");
       printf("  -v         Optional verbose flag.\n");
@@ -51,8 +60,8 @@ void parseArg(int argc, char *argv[]) {
       printf("  -t <file>  Trace file.\n");
       printf("\n");
       printf("Examples:\n");
-      printf("  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n");
-      printf("  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+      printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+      printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
       exit(1);
     case 'v':
       verbose = true;
@@ -70,105 +79,115 @@ void parseArg(int argc, char *argv[]) {
       traceFile = fopen(optarg, "r");
       break;
     default:
-      printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+      printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
       exit(1);
     }
   }
 }
 
 int simulator(void) {
-  // Create space for cache
-  int cacheSize = pow(2, setIndexBit);
-  cacheTable = malloc(sizeof(cacheData) * cacheSize);
-  // Return if malloc failed
-  if (cacheTable == NULL)
-    return -1;
-  for (int i = 0; i < setIndexBit; i++) {
-    // Create space for cache
-    cacheTable[i] = calloc(linePerSet, sizeof(cacheData));
-    // Return if malloc failed
-    if (cacheTable[i] == NULL)
-      return -1;
-  }
-
-  // Prepare variables for per-line operation
-  char buffer[20];
-  char operation;
-  uint64_t addr;
-  int size;
-
-  while (fgets(buffer, sizeof(buffer), traceFile) != NULL) {
-    if (buffer[0] == 'I') {
-      // Ignoring instruction load operation
-      continue;
-    } else {
-      int ret = 0;
-      sscanf(buffer, " %c %lx,%d", &operation, &addr, &size);
-      switch (operation) {
-      case 'S':
-        ret = checkCache(addr);
-        break;
-      case 'L':
-        ret = checkCache(addr);
-        break;
-      case 'M':
-        ret = checkCache(addr);
-        hits++;
-        break;
-      }
-      if (verbose) {
-        switch (ret) {
-        case 0:
-          printf("%c %lx,%d HIT.\n", operation, addr, size);
-          break;
-        case 1:
-          printf("%c %lx,%d MISS.\n", operation, addr, size);
-          break;
-        case 2:
-          printf("%c %lx,%d EVICTION.\n", operation, addr, size);
-          break;
+    int cacheSize = 1 << setIndexBit;
+    cache = malloc(cacheSize * sizeof(cacheSet));
+    // Quit function if malloc failed
+    if(cache == NULL)
+        return -1;
+    for(int i = 0; i < cacheSize; i++){
+        cache[i] = malloc(linePerSet * sizeof(cacheData));
+        // Quit function if malloc failed
+        if(cache[i] == NULL)
+            return -1;
+        for(int j = 0; j < linePerSet; j++){
+            // Initialize
+            cache[i][j].valid = 0;
+            cache[i][j].lru = 0;
+            cache[i][j].tag = 0;
         }
-      }
     }
-  }
-  for (int i = 0; i < setIndexBit; i++)
-    free(cacheTable[i]);
-  free(cacheTable);
-  fclose(traceFile);
-  return 0;
+    char buffer[20];
+    char operation;
+    uint64_t address;
+    int size;
+
+    int counter = 0;
+
+    while(fgets(buffer, sizeof(buffer), traceFile) != NULL){
+        counter++;
+        if(buffer[0] == 'I')
+            continue;
+        else{
+            sscanf(buffer, " %c %lx,%d", &operation, &address, &size);
+            unsigned int tag = parseTag(setIndexBit, blockOffsetBit, address);
+            unsigned int cacheset = parseCacheSet(setIndexBit, blockOffsetBit, address);
+            unsigned int offset = parseOffset(setIndexBit, blockOffsetBit, address);
+            switch(operation){
+                case 'L':
+                    checkCache(tag, cacheset, offset, counter);
+                    break;
+                case 'M':
+                    checkCache(tag, cacheset, offset, counter);
+                    checkCache(tag, cacheset, offset, counter);
+                    break;
+                case 'S':
+                    checkCache(tag, cacheset, offset, counter);
+                    break;
+            }
+        }
+    }
+    for(int i = 0; i < cacheSize; i++)
+        free(cache[i]);
+    free(cache);
+    return 0;
 };
 
-int checkCache(uint64_t address) {
-  uint64_t tag = address >> (setIndexBit + blockOffsetBit);
-  unsigned int setIndex = address >> blockOffsetBit & ((1 << setIndexBit) - 1);
+void checkCache(int tag, int cacheSet, int offset, int counter) {
+    for(int i = 0; i < linePerSet; i++){
+        if(cache[cacheSet][i].valid == 1 && cache[cacheSet][i].tag == tag){
+            cache[cacheSet][i].lru = counter;
+            hits++;
+            if(verbose)
+                printf("HIT\n");
+            return;
+        }
+    }
+    misses++;
+    if(verbose)
+        printf("MISS\n");
+    int n_lru = 0;
+    for(int i = 0; i < linePerSet; i++){
+        if(cache[cacheSet][i].valid == 0){
+            cache[cacheSet][i].valid = 1;
+            cache[cacheSet][i].tag = tag;
+            cache[cacheSet][i].lru = counter;
+            return;
+        }
+        else
+            if(cache[cacheSet][n_lru].lru > cache[cacheSet][i].lru)
+                n_lru = i;
+    }
 
-  int evict = 0;
-  int empty = -1;
-  cacheData *cacheSet = cacheTable[setIndex];
-  for (int i = 0; i < linePerSet; i++) {
-    if (cacheSet[i].valid) {
-      if (cacheSet[i].tag == tag) {
-        hits++;
-        cacheSet[i].lru = 1;
-        return 0;
-      }
-      cacheSet[i].lru++;
-      if (cacheSet[evict].lru <= cacheSet[i].lru) {
-        evict = i;
-      }
-    } else
-      empty = i;
-  }
-  misses++;
-  if (empty != -1) {
-    cacheSet[empty].valid = 1;
-    cacheSet[empty].tag = tag;
-    cacheSet[empty].lru = 1;
-    return 1;
-  } else {
-    cacheSet[evict].tag = tag;
-    cacheSet[evict].lru = 1;
     evictions++;
-    return 2;
-  }
+    if(verbose)
+        printf("EVICTION!\n");
+    cache[cacheSet][n_lru].valid = 1;
+    cache[cacheSet][n_lru].tag = tag;
+    cache[cacheSet][n_lru].lru = counter;
+    return;
+};
+unsigned int parseTag(int setIndexBit, int blockOffsetBit, int address){
+    unsigned maskOffset = (1 << blockOffsetBit) - 1;
+    unsigned maskSet = (1 << (setIndexBit + blockOffsetBit)) - (1 << blockOffsetBit);
+    unsigned maskTag = ~0 - (maskOffset + maskSet);
+    unsigned tag = (address & maskTag) >> (setIndexBit + blockOffsetBit);
+    return tag;
+};
+unsigned int parseCacheSet(int setIndexBit, int blockOffsetBit, int address){
+    unsigned maskSet = (1 << (setIndexBit + blockOffsetBit)) - (1 << blockOffsetBit);
+    unsigned cacheSet = (address & maskSet) >> (blockOffsetBit);
+    return cacheSet;
+
+};
+unsigned int parseOffset(int setIndexBit, int blockOffsetBit, int address){
+    unsigned maskOffset = (1 << blockOffsetBit) - 1;
+    unsigned offset = address & maskOffset;
+    return offset;
 };
